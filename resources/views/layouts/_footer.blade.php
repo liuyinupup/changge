fa26f5815446f609f4520627d8586985ea35d71e
vagrant <vagrant@homestead>
2019-07-21 05:29:06 +0000

    关于

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
   <div class="container" style="text-align: center">
     <p >
-      粤ICP备19070133号-1 | <a href="{{ route('articles.show',1) }}">关于我</a>
+      粤ICP备19070133号-1 | <a href="{{ route('articles.show',1) }}">关于</a>
     </p>
   </div>
 </footer>
