fe6310a8744ef26c5ca42f5410d3da938e5fdf2b
liuyin <liuyinupup@163.com>
2019-07-23 06:29:39 +0000

    markdown调整

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -16,7 +16,7 @@
         </div>
     </div>
     <div class="col-lg-3 d-none d-lg-block">
-        <aside class="sticky-top sm_shadow" style="z-index:-1;">
+        <aside class="sticky-top sm_shadow" style="z-index:0;">
             <div id="category" class="list-group">
             </div>
         </aside>
